80d2a230d49a6358d7d18c0972e5c3586fb6e2ad
0alessandrocicalese0 <alessandro.cicalese@hotmail.com>
2023-12-13 12:46:24 +0100

    Calcolo gruppi e integrale

--- hitachi_project.R
+++ hitachi_project.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(ggplot2)
 
-data <- readRDS("Projects/tr_13.rds")
+data <- readRDS("tr_13.rds")
 
 head(data)
 
@@ -16,35 +16,30 @@ ggplot(data = data[0:1000,]) +
   geom_line(mapping = aes(x = Timestamp, y = Speed))
 
 
-
-
-
-#! Mantenere la history e l'enviroment in modo tale da non dover ricaricare il tutto il tutto
-
-#* Integrale grafico Ampere per calcolare (A/h)
 # Creo una tabella contente solo i dati relativi alla Batteria 2
 subC2 <- data[, c("Timestamp", "Speed", "HMI_IBatt_C2", "HMI_VBatt_C2")]
 
 # Colonne: ID - Derivata - Fase
 subC2$ID       <- ifelse(data$HMI_IBatt_C2 >= 0,   1,  -1)
-subC2$Derivata <- c(0, diff(data$HMI_IBatt_C2) /15)
-subC2$Fasi     <- as.numeric(c(0, diff(data$Timestamp) > 15))
+subC2$Derivata <- c(0, diff(data$HMI_IBatt_C2) /diff(data$Timestamp))
+subC2$Periodo  <- cumsum(c(0, diff(data$Timestamp) > 15))
 
 
-# Contatore
-contatore_scarica <- 0
+# Contatore Gruppi
+gruppo <- rep(0, length(subC2$Timestamp))
 
-for (index in 2:(length(data$Timestamp) - 1000000)) {
-  if (data$Derivata_C2[index] < 0 
-      && subC2$ID[index] != subC2$ID[index-1] 
-      && subC2$Fasi_[index] != 1) {
-        contatore_scarica <- contatore_scarica + 1
+for (index in 2:(length(subC2$Timestamp))) {
+  if (subC2$Derivata[index] < 0 
+      && subC2$ID[index]      !=  subC2$ID[index-1] 
+      || subC2$Periodo[index] !=  subC2$Periodo[index-1]) {
+        gruppo[index] <-  1
+  }
+  else {
+    gruppo[index] <-  0
   }
 }
 
-# Visualizza il risultato
-print(contatore_scarica)
-
+subC2$Gruppo <- cumsum(gruppo)
 
 
 #* Visualizzare un ciclo di carica e scarica 
@@ -57,11 +52,26 @@ ggplot(data = data[1:740,]) +
 geom_line(mapping = aes(x = Timestamp, y = HMI_VBatt_C2)) +
   labs(title = "C2 Potential evolution", x = "date", y = "Voltage (V)") 
 
-ggplot(data = data[1:5000,]) +
-  geom_point(mapping = aes(x = Timestamp, y = HMI_IBatt_C2)) +
-  geom_hline(yintercept = 8.6, linetype = "dashed", color = "red") + 
-  labs(title = "", x = "date", y = "Ampere (I)")
+ggplot(data = subC2[1:5000,]) +
+  geom_line(mapping = aes(x = Timestamp, y = HMI_IBatt_C2), color = "black") +
+  geom_line(mapping = aes(x = Timestamp, y = Gruppo), color = 'red', size = 2) +
+  labs(title = "Primi 5000 dati", x = "Date", y = "Ampere (I)")
+
 
 ggplot(data = prova) +
   geom_point(mapping = aes(x = Timestamp, y = HMI_VBatt_C2)) +
   labs(title = "", x = "date", y = "Voltage (V)") 
+
+
+
+#* Integrale grafico Ampere per calcolare (A/h)
+capacità <- rep(0, length(subC2$Timestamp))
+for (index in 2:(length(subC2$Timestamp))) {
+  capacità[index] <- (subC2$Timestamp[index] - subC2$Timestamp[index-1])*(subC2$HMI_IBatt_C2[index] + subC2$HMI_IBatt_C2[index-1])/2
+}
+subC2$Capacità <- capacità
+
+
+
+
+save.image(file = "my_workspace.RData")
